97134eefa1468ba5e0ebeb1f13816f0053e15ef5
NarekBayunc <vip.bayunts@gmail.com>
2023-10-20 12:39:46 +0400

    Bug fix for cinema model attributes

--- SimpleMVC/Models/Cinema.cs
+++ SimpleMVC/Models/Cinema.cs
@@ -7,8 +7,12 @@ namespace SimpleMVC.Models
     {
         public int Id { get; set; }
         [Display(Name = "Cinema Logo")]
+        [Required(ErrorMessage = "Logo Picture is required")]
         public string? Logo { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must be between 3 and 50 characters ")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Description is required")]
         public string? Description { get; set; }
         public List<Movie>? Movies { get; set; }
 
